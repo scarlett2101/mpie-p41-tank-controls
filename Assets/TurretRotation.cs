0a3bce8abb8e19173570419cf2a76140ca6a7a47
scarlett2101 <xtn516@york.ac.uk>
2025-10-25 16:25:41 +0100

    done

--- Assets/TurretRotation.cs
+++ Assets/TurretRotation.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class TurretRotation : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float rotationSpeed = 50f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Q))
+        {
+            // rotate left
+            transform.Rotate(0, -rotationSpeed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            // rotate right
+            transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+        }
     }
 }
